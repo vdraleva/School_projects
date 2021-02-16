2a890fff65a6872a2e37b9c6883daeeef74a1984
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-02-16 12:27:46 +0200

    Add files via upload

---
+++ Tasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Най-често срещано число    
+
+            int n = 0;
+            int[] num = new int[100];
+            var line = Console.ReadLine();
+            var splitted = line.Split(' ');
+            foreach (var item in splitted)
+                num[n++] = int.Parse(item);
+
+            int[] counter = new int[100000];
+            for (int i = 0; i < n; i++)
+            {
+                counter[num[i]]++;
+            }
+            int max = -1000, maxi = 0;
+            for (int j = 0; j < 100000; j++)
+            {
+                if (counter[j] > max)
+                {
+                    max = counter[j];
+                    maxi = j;
+                }
+            }
+            Console.WriteLine($"Числото {maxi} е най-често срещаното (среща се {counter[maxi]} пъти)");
+        }
+    }
+}
