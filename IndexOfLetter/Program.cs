27dfd40d43331c8733bcf82f02eea4d3ebbe7887
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-02-16 12:29:18 +0200

    Add files via upload

---
+++ IndexOfLetter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOfLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Индекс на буква
+
+            string a = Console.ReadLine();
+            foreach (var item in a.ToString())
+                Console.WriteLine($"{item} -> {(int)item - 97}");
+        }
+    }
+}
