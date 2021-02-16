55240aea0bc8c1b77202945295347cc2e9933275
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-02-16 12:37:04 +0200

    Add files via upload

---
+++ RemoveTheNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача: Премахни числото
+             
+             List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+             int number = nums[nums.Count-1];
+             while (nums.Contains(number))
+             {
+                 nums.Remove(number);
+             }
+             Console.Write(string.Join(" ",nums));
+             Console.WriteLine();
+             
+        }
+    }
+}
