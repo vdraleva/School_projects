fb0a308802dc36f0be31e778935538e339d89fa4
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-02-16 12:38:53 +0200

    Add files via upload

---
+++ RemoveTheNegativeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTheNegativeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача: Изтриване на отрицателни елементи
+            
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    nums.Remove(nums[i]);
+                }
+            }
+            nums.Reverse();
+            Console.Write(string.Join(" ", nums));
+            Console.WriteLine();
+            
+        }
+    }
+}
