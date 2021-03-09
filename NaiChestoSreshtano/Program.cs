ff9ab68209c0094ff48d78642d4fcacd38943cc3
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-03-09 21:25:33 +0200

    Another school tasks
    The first one is to make an array into a number.
    The other task is for the most common number.

---
+++ NaiChestoSreshtano/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace NaiChestoSreshtano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int max = 0;
+            int repNum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                int currentNum = 0;
+
+                for (int j = i; j < myArray.Length; j++)
+                {
+                    if (myArray[i] == myArray[j])
+                    {
+                        currentNum++;
+                    }
+                }
+
+                if (currentNum > max)
+                {
+                    repNum = myArray[i];
+                    max = currentNum;
+                }
+            }
+
+            Console.WriteLine(repNum);
+        }
+    }
+}
---
+++ Preobrazuvane_Na_Masiv_V_Chislo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Preobrazuvane_Na_Masiv_V_Chislo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();          
+
+            for (int i = 0; i < myArr.Length - 1; i++)
+            {
+                int[] condensed = new int[myArr.Length - 1];
+
+                for (int j = 0; j < myArr.Length - 1; j++) 
+                {
+                    condensed[j] = myArr[j] + myArr[j + 1];
+                }
+
+                myArr = condensed;
+            }
+
+            foreach (int item in myArr)
+            {
+                Console.WriteLine(item);               
+            }
+        }
+    }
+}
