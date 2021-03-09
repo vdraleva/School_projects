09f2770ad32951922088e667b85026f253dc1a00
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-03-09 21:22:53 +0200

    Add files via upload

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int x = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = myArr.Length - 1;
+            int middle = (left + right) / 2;
+            while (left <= right && myArr[middle] != x)
+            {
+                if (myArr[middle] < x)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+                middle = (left + right) / 2;
+            }
+            if (myArr[middle] == x)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] b = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] c = new int[a.Length + b.Length];
+            int i = 0, j = 0, k = -1;
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c[++k] = a[i++];
+                }
+                else
+                {
+                    c[++k] = b[j++];
+                }
+            }
+            while (i < a.Length)
+            {
+                c[++k] = a[i++];
+            }
+            while (j < b.Length)
+            {
+                c[++k] = b[j++];
+            }
+            foreach (var item in c)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
