37f800713236dc31f5c33f143e59920f8b312932
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-09-16 21:39:42 +0300

    Add files via upload

---
+++ ListTasks/ElementsFromListAreDiff/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementsFromListAreDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се напише нерекурсивна (рекурсивна) функция,
+            //която проверява дали елементите на стек от числа са различни.
+            List<int> listOne = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < listOne.Count-1; i++)
+            {
+                for (int j = i+1; j < listOne.Count; j++)
+                {
+                    if (listOne[i]==listOne[j])
+                    {
+                        Console.WriteLine("there is a diff num");
+                        Console.WriteLine(listOne[i]);
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("there isn't diff num");
+            Console.WriteLine(string.Join(" ",listOne));
+        }
+    }
+}
---
+++ ListTasks/ListTasks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се напише нерекурсивна (рекурсивна) функция, 
+            //която проверява дали елемент принадлежи на списък(стек, опашка).
+            List<int> listOne = new List<int>();
+            listOne.Add(5);
+            listOne.Add(3);
+            listOne.Add(4);
+            listOne.Add(52);
+            listOne.Add(2);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < listOne.Count; i++)
+            {
+                if (listOne.Contains(n))
+                {
+                    Console.WriteLine("There is");
+                    return;
+                }
+            }
+            Console.WriteLine("There isn't");
+        }
+    }
+}
