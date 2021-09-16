d819dff18d4c03e26fd962d33f455d1250def445
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-09-16 21:42:11 +0300

    Add files via upload

---
+++ GreedyAlgorithms/GreedyAlgorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = {50,20,10,5,2,1 };
+            int sum = int.Parse(Console.ReadLine());
+            int i = 0; 
+            if (sum<=0)
+            {
+                Console.WriteLine("The number must be higher than 0");
+            }
+            while (sum!=0)
+            {
+                while (sum-coins[i]>=0)
+                {
+                    sum-=coins[i];
+                    Console.Write(coins[i]+", ");
+                }
+                i++;
+            }
+           
+        }
+    }
+}
