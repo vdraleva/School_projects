c9290699a1b8d5a23dd2081fd0985610b6a9a6d0
Viktoria Draleva <64305580+vdraleva@users.noreply.github.com>
2021-09-16 21:40:03 +0300

    Add files via upload

---
+++ StackTasks/StackContaintNum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackContaintNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stackOne = new Stack<int>();
+            stackOne.Push(5);
+            stackOne.Push(3);
+            stackOne.Push(4);
+            stackOne.Push(52);
+            stackOne.Push(2);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < stackOne.Count; i++)
+            {
+                
+                    if (stackOne[i] == stackOne[i+1])
+                    {
+                    Console.WriteLine("There is");
+                    return;
+                    
+                }
+                
+            }
+            Console.WriteLine("There isn't");
+        }
+    }
+}
---
+++ StackTasks/StackTasks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Stack<int> digits = new Stack<int>();
+            while (num!=0)
+            {
+                digits.Push(num%10);
+                num /= 10;
+            }
+            Console.WriteLine(string.Join("+",digits)+"="+digits.Sum());
+        }
+    }
+}
